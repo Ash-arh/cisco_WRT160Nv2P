260f10120eb1d6537fb73f175d412f35d3ce90c9
Ash-arh <ashrefalhor@yahoo.com>
2017-07-03 15:10:51 +0200

    color changes

--- wifi_password_viewer.py
+++ wifi_password_viewer.py
@@ -21,18 +21,19 @@ def megg():
 guu = Tk()
 guu.title("password viewer")
 guu.geometry("400x400")
-guu.configure(background='white')
+guu.configure(background="white")
 guu.resizable(width=False, height=False)
 
-nass1 = Label(guu, text="welcome to the wifi password viewer" , fg="black", bg="white")
-nass1.pack(fill=BOTH, expand=1)
+nass1 = Label(guu, text="welcome to the wifi password viewer" , fg="#cc0000" ,bg="white")
+nass1.pack()
 nass1.place(x=50, y=15)
-nass1.config(font=('times', 15, ' bold '))
+nass1.config(font=('Arial', 13, ' bold '))
 
-zer1 = Button(guu, text="Exit", height = 3, width = 25 ,command=guu.destroy)
+zer1 = Button(guu, text="Exit", height = 3, width = 25, fg="white", bg="#2196F3" ,command=guu.quit)
 zer1.pack(side=BOTTOM  ,padx = 3, pady = 25)
 
-zer2 = Button(guu, text="Show the password"  ,height = 3, width = 25, command= megg)
+
+zer2 = Button(guu, text="Show the password"  ,height = 3, width = 25 , fg="white", bg="#2196F3", command= megg)
 zer2.pack(side=LEFT)
 zer2.place(x=120, y=150)
 
